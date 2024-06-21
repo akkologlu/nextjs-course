1ffa800142f4e5ae16a7f08a353a3ad4596d136f
Abdullah Akkoloğlu <abdullahakkol@gmail.com>
2024-06-21 14:05:30 +0300

    foodies completed

---
+++ 05-foodies-nextjs/app/meals/share/error.js
@@ -0,0 +1,11 @@
+"use client";
+const Error = () => {
+  return (
+    <main className="error">
+      <h1>An eror occured!</h1>
+      <p>Failed to create meal. Please try again later.</p>
+    </main>
+  );
+};
+
+export default Error;
